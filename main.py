be2265bba7d8f5a092cba1731f29f2108fb59b62
ALEX GATIEV <maiksanchos@gmail.com>
2023-10-23 18:39:46 +0300

    fix: README.md

--- main.py
+++ main.py
@@ -11,17 +11,21 @@ def send_welcome(message):
 	bot.reply_to(message, "Howdy, how are you doing?")
 
 
+
+
+
 @bot.message_handler(func=lambda m: True)
+
+
+
 def echo_all(message):
-	bot.reply_to(message, message.text)
+	markup = telebot.types.ReplyKeyboardMarkup(row_width=2)
+	but1 = telebot.types.KeyboardButton('a')
+	but2 = telebot.types.KeyboardButton('v')
+	but3 = telebot.types.KeyboardButton('d')
+	markup.add(*args:but1, but2, but3)
+
+bot.send_message(message.chat.id, message.text,reply_markup=markup)
 
 
 bot.infinity_polling()
-def echo_all():
-	markup = telebot.types.ReplyKeyboardMarkup(row_width=2)
-	itembtn1 = telebot.types.KeyboardButton('a')
-	itembtn2 = telebot.types.KeyboardButton('v')
-	itembtn3 = telebot.types.KeyboardButton('d')
-	markup.add(itembtn1, itembtn2, itembtn3)
-
-bot.send_message(message.chat.id, message.text,reply_markup=markup)
