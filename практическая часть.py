807615ffe930cf57fefcec5766feadc4d9e0a422
ALEX GATIEV <maiksanchos@gmail.com>
2023-12-25 18:56:52 +0300

    fix: README.md

--- практическая часть.py
+++ практическая часть.py
@@ -3,17 +3,16 @@ class Wizard:
         try:
             if len(name) < 3:
                 raise ValueError("Имя  меньше 3 символов")
-
+                print('Имя  меньше 3 символов')
             if health_points <= 0:
                 raise ValueError("Здоровье меньше 0")
-
+                print('Здоровье меньше 0')
             if mana_points < 0:
                 raise ValueError("Мана меньше 0")
-        except ValueError as e:
-            print(e)
+                print('Мана меньше 0')
+        except ValueError :
+            print('всё плохо')
         else:
             print('всё ок')
         finally:
             print("удачи вам")
-
-w = Wizard('kkkk', 1, -1)
