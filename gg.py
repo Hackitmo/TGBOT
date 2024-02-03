b2cf45cf4bc7f43713069609e1368ca8c5a87f45
Hackitmo <146744457+Hackitmo@users.noreply.github.com>
2024-02-03 22:35:05 +0300

    Update gg.py

--- gg.py
+++ gg.py
@@ -1,27 +1,10 @@
-class Pokemon:
-    def eng(self,id,type,species,hp,speed,attack,defense,experience):
-        self.id=1
-        self.type = 'trans'
-        self.species='rg'
-        self.hp=100
-        self.speed=25
-        self.attack=15
-        self.defense=45
-        self.experience=0
-        def fight(self,others):
-            first, second =None,None
-            if self.speed>other.speed:
-                first,second=self,others
-            else:
-                first,second=others,self
-            while self.hp>0 and others.hp >0:
-                damage=max(first.attack-second.defense, 0)
-                second.hp-=damage
-                first,second=second,first
-
-rg=Pokemon
-print(rg.hp)
-scorbunny
-another=Pokemon()
-anothaer.speed=30
-esult
+import sqlite3
+conn = sqlite3.connect('chat_history.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS chat_history
+            (user_message TEXT, bot_message TEXT)''')
+c.execute('SELECT * FROM chat_history')
+a=c.fetchall()
+for i in a:
+    print(i)
+conn.close()
