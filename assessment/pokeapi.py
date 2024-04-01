9aafbf153500f40b48c7201dcbc54419332e3919
Your Name <maiksanchos@gmail.com>
2024-04-01 15:09:17 +0300

    Merge remote-tracking branch 'origin/main'

--- assessment/pokeapi.py
+++ assessment/pokeapi.py
@@ -1,14 +1,15 @@
 from pokemon import Pokemon
 from base_pokemon import BasePokemon
-
+import requests
 class PokeAPI:
-    def get_pokemon(name_or_id: str) -> Pokemon:
-
-        return Pokemon(name=name_or_id, id=25, height=1.6, weight=85.6)
+    @staticmethod
+    def get_pokemon (name_or_id: str)-> Pokemon:
+        pokemons=requests.get(f'https://pokeapi.co/api/v2/pokemon/{name_or_id}').json()
+        return Pokemon(pokemons['name'], pokemons['id'], pokemons['height'], pokemons['weight'])
+    @staticmethod
     def get_all(get_full: bool = False):
-
         for i in range(1, 51):
             if get_full:
-                yield PokeAPI.get_pokemon(i)
+                yield PokeAPI.get_pokemon(1)
             else:
-                yield BasePokemon(name=f"Pokemon{i}")
+                yield BasePokemon (name=f"Pokemon{i}")
