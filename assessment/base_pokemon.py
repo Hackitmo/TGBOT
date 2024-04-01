611e98d050fed5867bca942ed343bb5144048c64
Your Name <maiksanchos@gmail.com>
2024-04-01 15:07:46 +0300

    Merge remote-tracking branch 'origin/main'

---
+++ assessment/base_pokemon.py
@@ -0,0 +1,8 @@
+from pokemon import Pokemon
+
+class BasePokemon:
+    def __init__(self, name: str):
+        self.name = name
+
+    def __str__(self) -> str:
+        return f"Pokemon: {self.name}"
---
+++ assessment/pokemon.py
@@ -0,0 +1,9 @@
+class Pokemon:
+    def __init__(self, name: str, id: int, height: float, weight: float):
+        self.name = name
+        self.id = id
+        self.height = height
+        self.weight = weight
+
+    def __str__(self) -> str:
+        return f"Pokemon: {self.name}, ID: {self.id}, Height: {self.height}, Weight: {self.weight}"
