c523bac5f25377d34432d7fba2f0e8083d713e91
SanyKing239 <146744457+Hackitmo@users.noreply.github.com>
2025-05-30 14:33:47 +0300

    Update практическая часть.py

--- практическая часть.py
+++ практическая часть.py
@@ -1,18 +1,1 @@
-class Wizard:
-    def __init__(self, name, health_points, mana_points):
-        try:
-            if len(name) < 3:
-                raise ValueError("Имя  меньше 3 символов")
-                print('Имя  меньше 3 символов')
-            if health_points <= 0:
-                raise ValueError("Здоровье меньше 0")
-                print('Здоровье меньше 0')
-            if mana_points < 0:
-                raise ValueError("Мана меньше 0")
-                print('Мана меньше 0')
-        except ValueError :
-            print('всё плохо')
-        else:
-            print('всё ок')
-        finally:
-            print("удачи вам")
+
